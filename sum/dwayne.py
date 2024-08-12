a8268372dcd18e5f3dff3c89c2581b7ea88ce2af
Dwayne <burrows.dwayne@icloud.com>
2024-08-12 12:04:20 +0200

    add solvers solve for sum

--- sum/dwayne.py
+++ sum/dwayne.py
@@ -2,5 +2,5 @@ def sum(n):
     total = 0
     for i in range(n + 1):
         total += i
-    return total
+    return (total)
 print(sum(15))
---
+++ sum/solver.py
@@ -0,0 +1,3 @@
+n = int(input('type number: '))
+summ = int((n/2)*(n+1))
+print(f'sum of all numbers till {n} is {summ}')
