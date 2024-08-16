57248f38e721f3e71e6151d698cac630b0246262
Dwayne <burrows.dwayne@icloud.com>
2024-08-16 15:10:12 +0200

    Solve for the longest number challenge

---
+++ Largest_num/dwayne.py
@@ -0,0 +1,12 @@
+# input = [3, 34, 30, 8, 9]
+input = [8, 1, 4]
+
+# sort_list = sorted(input, reverse=True)
+
+# res = ''.join(str(n) for n in sort_list)
+
+# print(res)
+
+sort = ''.join(str(n) for n in sorted(input, reverse=True))
+
+print(sort)
