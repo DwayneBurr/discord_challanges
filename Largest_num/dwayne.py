ac354fb2759e7c864edf7744b83e2d63f18b0a44
Dwayne <burrows.dwayne@icloud.com>
2024-08-16 20:54:33 +0200

    Largest number refactors

--- Largest_num/dwayne.py
+++ Largest_num/dwayne.py
@@ -1,12 +1,21 @@
-# input = [3, 34, 30, 8, 9]
-input = [8, 1, 4]
+input = [7, 34, 9]
+# input = [8, 1, 4]
 
+# first attempt
 # sort_list = sorted(input, reverse=True)
 
 # res = ''.join(str(n) for n in sort_list)
 
 # print(res)
 
-sort = ''.join(str(n) for n in sorted(input, reverse=True))
+# refactor # 1
+# sort = ''.join(str(n) for n in sorted(input, reverse=True))
 
-print(sort)
+# print(sort)
+
+# spent some time making it better for a second time
+def longest(input: list[int]) -> str:
+    sort = ''.join(str(n) for n in sorted(input, reverse=True))
+    return sort
+res = longest(input)
+print(res)
