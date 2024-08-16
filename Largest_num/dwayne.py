94f3a3da410ddab1956e56856aa204c6182398e1
Dwayne <burrows.dwayne@icloud.com>
2024-08-16 21:10:50 +0200

    Added individual numbers from highest to lowest

--- Largest_num/dwayne.py
+++ Largest_num/dwayne.py
@@ -14,8 +14,22 @@ input = [7, 34, 9]
 # print(sort)
 
 # spent some time making it better for a second time
-def longest(input: list[int]) -> str:
-    sort = ''.join(str(n) for n in sorted(input, reverse=True))
-    return sort
-res = longest(input)
+# def longest(input: list[int]) -> str:
+#     sort = ''.join(str(n) for n in sorted(input, reverse=True))
+#     return sort
+# res = longest(input)
+# print(res)
+
+#sort numbers individually from largest to smallest
+def largest(input: list[int]) -> str:
+    digits = []
+    for i in input:
+        for d in str(i):
+            digits.append(d)
+
+    sort_digits = sorted(digits, reverse=True)
+    sort_digits = ''.join(sort_digits) 
+    return sort_digits
+
+res = largest(input)
 print(res)
